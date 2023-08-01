9faae0e12a71d147174575f1c8c4329d1ed08ecb
Nonthaphat Boonjua <amcpethai22@gmail.com>
2023-08-01 13:24:26 +0700

    update element

--- app.py
+++ app.py
@@ -6,11 +6,19 @@ from ffmpy import FFmpeg
 import http.server
 import socketserver
 import threading
+import unicodedata
+import re
+import urllib.parse
 
 
 app = Flask(__name__)
 CORS(app)
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def normalize_video_title(video_title):
+    normalized_title = unicodedata.normalize('NFKD', video_title).encode('ASCII', 'ignore').decode('ASCII')
+
+    return re.sub(r'[^\x00-\x7F]', '', normalized_title)
 
 @app.route('/download', methods=['POST'])
 def download_video():
@@ -23,6 +31,10 @@ def download_video():
 
         video_title = yt.title
 
+        video_title_normalized = normalize_video_title(video_title)
+
+        file_extension = '' 
+
         if 'video' in audio_format:
             video_stream = yt.streams.filter(res=resolution, file_extension='mp4').first()
             video_stream.download('downloads/')
@@ -30,14 +42,17 @@ def download_video():
             audio_stream = yt.streams.filter(only_audio=True).filter(file_extension='webm').first()
             audio_stream.download('downloads/')
             convert_to_flac(yt.title)
+            file_extension = 'flac'
         elif audio_format == 'mp3':
             audio_stream = yt.streams.filter(only_audio=True).filter(file_extension='mp4').first()
             audio_stream.download('downloads/')
+            file_extension = 'mp3'
             convert_to_mp3(yt.title)
         elif audio_format == 'wav':
             audio_stream = yt.streams.filter(only_audio=True).filter(file_extension='webm').first()
             audio_stream.download('downloads/')
             convert_to_wav(yt.title)
+            file_extension = 'wav'
         else:
             return jsonify({'error': 'Invalid audio format.'}), 400
 
@@ -47,12 +62,14 @@ def download_video():
 
         os.remove(file_path)
 
+        encoded_filename = urllib.parse.quote(f'{video_title_normalized}.{file_extension}')
+
         return Response(
             response=response_file,
             status=200,
             headers={
                 'Content-Type': f'audio/{file_extension}',
-                'Content-Disposition': f'attachment; filename="{video_title}.{file_extension}"',
+                'Content-Disposition': f'attachment; filename*=utf-8\'\'{encoded_filename}',
                 'x-video-title': video_title 
             }
         )
@@ -84,26 +101,27 @@ def convert_to_wav(title):
     ff.run()
     os.remove(input_file)
 
-def serve_frontend():
-    try:
-        PORT = 8000
+# def serve_frontend():
+#     try:
+#         PORT = 8000
 
 
-        os.chdir(os.path.join(ROOT_DIR, 'web'))
+#         os.chdir(os.path.join(ROOT_DIR, 'web'))
 
 
-        handler = http.server.SimpleHTTPRequestHandler
-        with socketserver.TCPServer(("", PORT), handler) as httpd:
-            print(f"Frontend server running on http://localhost:{PORT}/")
-            httpd.serve_forever()
-    except Exception as e:
-        print(f"Error serving frontend: {str(e)}")
+#         handler = http.server.SimpleHTTPRequestHandler
+#         with socketserver.TCPServer(("", PORT), handler) as httpd:
+#             print(f"Frontend server running on http://localhost:{PORT}/")
+#             httpd.serve_forever()
+#     except Exception as e:
+#         print(f"Error serving frontend: {str(e)}")
 
 if __name__ == '__main__':
+    app.run()
 
-    flask_thread = threading.Thread(target=app.run)
-    flask_thread.start()
+    # flask_thread = threading.Thread(target=app.run)
+    # flask_thread.start()
 
   
-    frontend_thread = threading.Thread(target=serve_frontend)
-    frontend_thread.start()
+    # frontend_thread = threading.Thread(target=serve_frontend)
+    # frontend_thread.start()
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const [url, setUrl] = useState<string>('');
   const [audioFormat, setAudioFormat] = useState<string>('mp3');
   const [message, setMessage] = useState<string>('');
   const [loading, setLoading] = useState<boolean>(false);
+
 
   const handleDownload = async () => {
     setLoading(true);
@@ -17,6 +20,7 @@ function App() {
 
       const fileExtension = audioFormat === 'mp3' ? 'mp3' : audioFormat === 'flac' ? 'flac' : 'wav';
       const blob = new Blob([response.data], { type: `audio/${fileExtension}` });
+
 
       const fileName = response.headers['x-video-title'];
 
@@ -31,8 +35,8 @@ function App() {
       document.body.removeChild(a);
 
       setMessage('Download completed successfully.');
-    } catch (error) {
-      setMessage('Error: Something went wrong.');
+    } catch (error: any) {
+      setMessage('Error: ' + error.response?.data?.error || 'Something went wrong.');
     } finally {
       setLoading(false);
     }
@@ -41,6 +45,7 @@ function App() {
 
   return (
     <div className='w-screen flex flex-col items-center justify-center'>
+
       <div className='flex'>
         <b className="text-2xl font-semibold mb-4 dark:text-white -tracking-wide">YouTube</b>
         <p className="text-2xl font-light mb-4 dark:text-white -tracking-wide">Downloader</p>
@@ -57,46 +62,72 @@ function App() {
         <button
           onClick={handleDownload}
           disabled={loading}
-          className={`px-4 py-1 flex items-center stroke-black hover:stroke-white bg-[#ffffff] text-black rounded-r-full shadow-md hover:bg-[#000000] hover:text-white focus:outline-none focus:ring-2 focus:bg-[#2e2e2e] ${loading ? 'opacity-50 cursor-not-allowed' : '' // Reduce opacity and disable cursor during download/conversion
+          className={`px-4 py-1 flex items-center stroke-black hover:stroke-white bg-[#ffffff] text-black rounded-r-full shadow-md hover:bg-[#000000] hover:text-white focus:outline-none focus:ring-2 focus:bg-[#2e2e2e] ${loading ? 'opacity-100 cursor-not-allowed bg-[#000000]' : ''
             }`}
         >
-
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width={800}
-            height={800}
-            fill="none"
-            viewBox="0 0 24 24"
-            className='h-6 w-6'
-
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M6 21h12M12 3v14m0 0 5-5m-5 5-5-5"
-            />
-          </svg>
+          {loading ? (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              className="animate-spin h-6 w-6"
+              fill="none"
+              stroke="#ffffff"
+            >
+              <circle cx="12" cy="12" r="10" stroke="#ffffff" strokeWidth="2" />
+              <path
+                fill="#ffffff"
+                d="M12 6v6l4 2"
+              />
+            </svg>
+          ) : (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={800}
+              height={800}
+              fill="none"
+              viewBox="0 0 24 24"
+              className="h-6 w-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M6 21h12M12 3v14m0 0 5-5m-5 5-5-5"
+              />
+            </svg>
+          )}
         </button>
       </div>
       <br />
       <div className="">
-          <label className="dark:text-white">
-            <p className=' tracking-widest uppercase text-[11px] font-bold'>Audio Format</p>
-            <select
-              value={audioFormat}
-              onChange={(e) => setAudioFormat(e.target.value)}
-              className="px-4 py-3 border-[#1b1b1b] bg-[#0e0e0e] outline-none dark:border-white placeholder:text-sm"
-            >
-              <option value="mp3">MP3</option>
-              <option value="wav">WAV</option>
-              <option value="flac">FLAC</option>
-            </select>
-            
-          </label>
-          &nbsp;&nbsp;&nbsp;
-        </div>
-      {message && <code className="mt-4 text-white bg-black p-3 rounded-lg">{message}</code>}
+        <label className="dark:text-white">
+          <p className=' tracking-widest uppercase text-[11px] font-bold'>Audio Format</p>
+          <select
+            value={audioFormat}
+            onChange={(e) => setAudioFormat(e.target.value)}
+            className="px-4 py-3 border-[#1b1b1b] bg-[#0e0e0e] outline-none dark:border-white placeholder:text-sm"
+          >
+            <option value="mp3">MP3</option>
+            <option value="wav">WAV</option>
+            <option value="flac">FLAC</option>
+          </select>
+
+        </label>
+        &nbsp;&nbsp;&nbsp;
+      </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      {message && toast(message)}
     </div>
   );
 }
